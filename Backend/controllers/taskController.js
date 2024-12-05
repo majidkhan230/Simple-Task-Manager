8320b536dbe17657ac1be62208f4b5af80e87504
MajidKh230 <khanmajid230@gmail.com>
2024-12-05 23:32:58 +0500

    work on controller

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -1,24 +1,68 @@
-import express from 'express'
+import express from "express";
 import fs from "fs";
+import { get } from "https";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "../data", "data.json");
+// console.log(filePath);
 
-
-
-
+// readTask
 export const readTask = () => {
+  const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-
-
-  const filePath = path.join(__dirname, "../data", "data.json");
-  console.log(filePath);
-
-  const data = fs.readFileSync(filePath,'utf-8');
-
-  console.log(data);
+  return data;
 };
+
+// writeTask
+export const writeTask = (task) => {
+  const tasks = readTask();
+  tasks.push(task);
+  fs.writeFileSync(filePath, JSON.stringify(tasks));
+};
+
+// getAllTask
+export const getAllData = (req,res)=>{
+  try {
+    const data =  readTask()
+     res.status(200).send({
+         message:"welcome to task data",
+         task: data
+     })
+ } catch (error) {
+     res.status(500).send({
+         message:"some thing went wrong with server",
+         error: error.message
+     })
+ }
+}
+
+
+//createTask
+export const createTask  = (req,res)=>{
+  try {
+  const task =req.body
+  writeTask(task)
+
+     res.status(201).send({
+         message:"task created sucessfully",
+         task: task
+     })
+ } catch (error) {
+     res.status(500).send({
+         message:"some thing went wrong with server",
+         error: error.message
+     })
+ }
+}
+
+export const updateTask = (req,res)=>{
+  const id = req.params.id
+
+  const data = readTask()
+  const filterData = data.find((item)=>item.id == id)
+  console.log(filterData)
+
+}
--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,22 +1,11 @@
 import express from 'express'
-import { readTask } from '../controllers/taskcontroller.js';
+import { createTask, getAllData, readTask, updateTask, writeTask } from '../controllers/taskController.js';
 
 
 const taskRoutes = express();
 
 
-taskRoutes.get("/",(req,res)=>{
-    try {
-        readTask()
-        res.status(200).send({
-            message:"welcome to task",
-        })
-    } catch (error) {
-        res.status(500).send({
-            message:"some thing went wrong with server",
-            error: error.message
-        })
-    }
-})
-
+taskRoutes.get("/",getAllData)
+taskRoutes.post("/",createTask)
+taskRoutes.put("/:id",updateTask)
 export default taskRoutes
